21c81eb5fce6cbf96003653e91eed0110f109ee9
21057 <2105798885@qq.com>
2022-11-04 12:39:31 +0800

    add LC-75

---
+++ TwoPointers-双指针/75. 颜色分类/main.go
@@ -0,0 +1,57 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/11/4
+ * Time: 0:42
+ * Description: No Description
+ */
+
+func main() {
+}
+
+func sortColors1(nums []int) {
+	count := swapColors(nums, 0)
+	swapColors(nums[count:], 1)
+}
+
+func swapColors(nums []int, target int) (countTarget int) {
+	for i, c := range nums {
+		if c == target {
+			nums[i], nums[countTarget] = nums[countTarget], nums[i]
+			countTarget++
+		}
+	}
+	return
+}
+
+func sortColors2(nums []int) {
+	p0, p1 := 0, 0
+	for i, c := range nums {
+		if c == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			if p0 < p1 {
+				nums[i], nums[p1] = nums[p1], nums[i]
+			}
+			p0++
+			p1++
+		} else if c == 1 {
+			nums[i], nums[p1] = nums[p1], nums[i]
+			p1++
+		}
+	}
+}
+
+func sortColors(nums []int) {
+	p0, p2 := 0, len(nums)-1
+	for i := 0; i <= p2; i++ {
+		for ; i <= p2 && nums[i] == 2; p2-- {
+			nums[i], nums[p2] = nums[p2], nums[i]
+		}
+		if nums[i] == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			p0++
+		}
+	}
+}
