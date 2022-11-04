52ce8d542e1e7409410c508813318e1dfcf6eba3
21057 <2105798885@qq.com>
2022-11-04 21:22:12 +0800

    add LC-67

---
+++ Factorial-阶乘/67. 二进制求和/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "strconv"
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/11/4
+ * Time: 21:21
+ * Description: No Description
+ */
+
+func addBinary(a string, b string) string {
+	ans := ""
+	carry := 0
+	lenA, lenB := len(a), len(b)
+	n := max(lenA, lenB)
+
+	for i := 0; i < n; i++ {
+		if i < lenA {
+			carry += int(a[lenA-i-1] - '0')
+		}
+		if i < lenB {
+			carry += int(b[lenB-i-1] - '0')
+		}
+		ans = strconv.Itoa(carry%2) + ans
+		carry /= 2
+	}
+	if carry > 0 {
+		ans = "1" + ans
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
