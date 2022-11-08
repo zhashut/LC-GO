b19267d8802c9090fe0e6bc6ef96b884724502bc
21057 <2105798885@qq.com>
2022-11-09 01:53:15 +0800

    add LC-977

---
+++ Array-数组/977. 有序数组的平方/main.go
@@ -0,0 +1,60 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/11/9
+ * Time: 1:04
+ * Description: No Description
+ */
+
+func main() {
+	nums := []int{-4, -1, 0, 3, 10}
+	sortedSquares(nums)
+}
+
+func sortedSquares(nums []int) []int {
+	n := len(nums)
+	lastNegIndex := -1
+	for i := 0; i < n && nums[i] < 0; i++ {
+		lastNegIndex = i
+	}
+
+	ans := make([]int, 0, n)
+	for i, j := lastNegIndex, lastNegIndex+1; i >= 0 || j < n; {
+		if i < 0 {
+			ans = append(ans, nums[j]*nums[j])
+			j++
+		} else if j == n {
+			ans = append(ans, nums[i]*nums[i])
+			i--
+		} else if nums[i]*nums[i] < nums[j]*nums[j] {
+			ans = append(ans, nums[i]*nums[i])
+			i--
+		} else {
+			ans = append(ans, nums[j]*nums[j])
+			j++
+		}
+	}
+
+	return ans
+}
+
+// 双指针
+func sortedSquares1(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+
+	i, j := 0, n-1
+
+	for pos := n - 1; pos >= 0; pos-- {
+		if v, w := nums[i]*nums[i], nums[j]*nums[j]; v > w {
+			ans[pos] = v
+			i++
+		} else {
+			ans[pos] = w
+			j--
+		}
+	}
+	return ans
+}
