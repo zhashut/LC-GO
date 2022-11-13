ae535a73cf076af7ea09f74c23dc88a72fdc0381
21057 <2105798885@qq.com>
2022-11-13 21:53:41 +0800

    add LC-剑指Offer-29

---
+++ matrix-矩阵/剑指 Offer 29. 顺时针打印矩阵/main.go
@@ -0,0 +1,52 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/11/13
+ * Time: 21:51
+ * Description: No Description
+ */
+
+func main() {
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	spiralOrder(matrix)
+}
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+	var (
+		rows, columns            = len(matrix), len(matrix[0])
+		order                    = make([]int, rows*columns)
+		index                    = 0
+		left, right, top, bottom = 0, columns - 1, 0, rows - 1
+	)
+
+	for left <= right && top <= bottom {
+		for column := left; column <= right; column++ {
+			order[index] = matrix[top][column]
+			index++
+		}
+		for row := top + 1; row <= bottom; row++ {
+			order[index] = matrix[row][right]
+			index++
+		}
+		if left < right && top < bottom {
+			for column := right - 1; column > left; column-- {
+				order[index] = matrix[bottom][column]
+				index++
+			}
+			for row := bottom; row > top; row-- {
+				order[index] = matrix[row][left]
+				index++
+			}
+		}
+		left++
+		right--
+		top++
+		bottom--
+	}
+	return order
+}
