c139e05f78a3b4ea051e46b015f5332587b3d382
21057 <2105798885@qq.com>
2022-11-17 08:39:54 +0800

    add LC-142

---
+++ Linked-链表/142. 环形链表 II/main.go
@@ -0,0 +1,54 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/11/17
+ * Time: 8:11
+ * Description: No Description
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+// 方法一：哈希表
+func detectCycle(head *ListNode) *ListNode {
+	ans := map[*ListNode]int{}
+	for node := head; node != nil; node = node.Next {
+		if _, ok := ans[node]; ok {
+			return node
+		}
+		ans[node]++
+	}
+
+	return nil
+}
+
+// 快慢指针
+func detectCycle1(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
