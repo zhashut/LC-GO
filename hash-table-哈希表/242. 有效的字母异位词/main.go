7b91ace165e80c427e5c1d21b3eaaaff15d09033
21057 <2105798885@qq.com>
2022-11-18 11:52:57 +0800

    add LC-242

---
+++ hash-table-哈希表/242. 有效的字母异位词/main.go
@@ -0,0 +1,36 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/11/18
+ * Time: 11:39
+ * Description: No Description
+ */
+
+func main() {
+	s, v := "anagram", "nagaraam"
+	isAnagram(s, v)
+}
+
+func isAnagram(s string, t string) bool {
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	cnt := map[rune]int{}
+
+	for _, val := range s {
+		cnt[val]++
+	}
+
+	for _, val := range t {
+		cnt[val]--
+		if cnt[val] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
