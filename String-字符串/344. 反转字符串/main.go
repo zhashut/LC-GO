38d6b649d3157dc9a5af0ec9e22dc98d9b564544
21057 <2105798885@qq.com>
2022-11-25 00:10:17 +0800

    add LC-344

--- String-字符串/344. 反转字符串/main.go
+++ String-字符串/344. 反转字符串/main.go
@@ -13,6 +13,14 @@ func main() {
 }
 
 func reverseString(s []byte) {
+	for i, j := 0, len(s)-1; i < j; {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
+	}
+}
+
+func reverseString1(s []byte) {
 	rever := []byte{}
 
 	for _, val := range s {
