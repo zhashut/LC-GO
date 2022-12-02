ac12060045cd74cdce372b2682488e1f429b1e41
21057 <2105798885@qq.com>
2022-12-02 20:30:47 +0800

    add LC-150. 逆波兰表达式求值

---
+++ Stack-栈/150. 逆波兰表达式求值/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "strconv"
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/12/2
+ * Time: 19:43
+ * Description: No Description
+ */
+
+func main() {
+
+}
+
+func evalRPN(tokens []string) int {
+	var stack []int
+
+	for _, token := range tokens {
+		val, err := strconv.Atoi(token)
+		if err == nil {
+			stack = append(stack, val)
+		} else {
+			num1, num2 := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			switch token {
+			case "+":
+				stack = append(stack, num1+num2)
+			case "-":
+				stack = append(stack, num1-num2)
+			case "*":
+				stack = append(stack, num1*num2)
+			case "/":
+				stack = append(stack, num1/num2)
+			}
+		}
+	}
+	return stack[0]
+}
