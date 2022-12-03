7738a9f5064f626975892665b8ed80200faf2760
21057 <2105798885@qq.com>
2022-12-03 23:43:11 +0800

    add LC-239. 滑动窗口最大值

---
+++ Stack-栈/239. 滑动窗口最大值/main.go
@@ -0,0 +1,94 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/12/3
+ * Time: 23:20
+ * Description: No Description
+ */
+
+func main() {
+
+}
+
+// 解法一：封装单调队列的方式解题
+func maxSlidingWindow(nums []int, k int) []int {
+	queue := NewMyQueue()
+	length := len(nums)
+	res := make([]int, 0)
+	// 先将前k个元素放入队列
+	for i := 0; i < k; i++ {
+		queue.Push(nums[i])
+	}
+	// 记录前k个元素的最大值
+	res = append(res, queue.Front())
+
+	for i := k; i < length; i++ {
+		// 滑动窗口移除最前面的元素
+		queue.Pop(nums[i-k])
+		// 滑动窗口添加最后面的元素
+		queue.Push(nums[i])
+		// 记录最大值
+		res = append(res, queue.Front())
+	}
+	return res
+}
+
+// MyQueue
+type MyQueue struct {
+	queue []int
+}
+
+func NewMyQueue() *MyQueue {
+	return &MyQueue{
+		queue: make([]int, 0),
+	}
+}
+
+func (m *MyQueue) Front() int {
+	return m.queue[0]
+}
+
+func (m *MyQueue) Back() int {
+	return m.queue[len(m.queue)-1]
+}
+
+func (m *MyQueue) Empty() bool {
+	return len(m.queue) == 0
+}
+
+func (m *MyQueue) Push(val int) {
+	for !m.Empty() && val > m.Back() {
+		m.queue = m.queue[:len(m.queue)-1]
+	}
+	m.queue = append(m.queue, val)
+}
+
+func (m *MyQueue) Pop(val int) {
+	if !m.Empty() && val == m.Front() {
+		m.queue = m.queue[1:]
+	}
+}
+
+// 解法二 双端队列 Deque
+func maxSlidingWindow1(nums []int, k int) []int {
+	if len(nums) == 0 || len(nums) < k {
+		return make([]int, 0)
+	}
+	window := make([]int, 0, k) // store the index of nums
+	result := make([]int, 0, len(nums)-k+1)
+	for i, v := range nums { // if the left-most index is out of window, remove it
+		if i >= k && window[0] <= i-k {
+			window = window[1:len(window)]
+		}
+		for len(window) > 0 && nums[window[len(window)-1]] < v { // maintain window
+			window = window[0 : len(window)-1]
+		}
+		window = append(window, i) // store the index of nums
+		if i >= k-1 {
+			result = append(result, nums[window[0]]) // the left-most is the index of max value in nums
+		}
+	}
+	return result
+}
