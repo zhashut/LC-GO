6cde6c7899ba6b3fbca3c9c781c6b17fce2de31c
21057 <2105798885@qq.com>
2022-12-10 01:38:41 +0800

    add LC-二叉树的递归遍历

---
+++ BinaryTree-二叉树/二叉树的递归遍历/mian.go
@@ -0,0 +1,64 @@
+package main
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/12/10
+ * Time: 1:07
+ * Description: No Description
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+// 前序遍历
+func preorderTraversal(root *TreeNode) (res []int) {
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		res = append(res, node.Val)
+		traversal(node.Left)
+		traversal(node.Right)
+	}
+	traversal(root)
+	return res
+}
+
+// 中序遍历
+func inorderTraversal(root *TreeNode) (res []int) {
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		traversal(node.Left)
+		res = append(res, node.Val)
+		traversal(node.Right)
+	}
+	traversal(root)
+	return res
+}
+
+// 后续遍历
+func postorderTraversal(root *TreeNode) (res []int) {
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		traversal(node.Left)
+		traversal(node.Right)
+		res = append(res, node.Val)
+	}
+	traversal(root)
+	return res
+}
