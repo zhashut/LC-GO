70c41874cf7b24b38e9d134b7e8ea9a51b836f49
2105798885@qq.com <2105798885@qq.com>
2022-12-14 22:32:25 +0800

    add LC-全局TreeNode

---
+++ BinaryTree-二叉树/global/TreeNode.go
@@ -0,0 +1,15 @@
+package global
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/12/14
+ * Time: 22:07
+ * Description: No Description
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
