7c9b97f6163a68e89e0e5058662af17b4582ff8a
2105798885@qq.com <2105798885@qq.com>
2022-12-25 21:25:07 +0800

    add LC-513. 找树左下角的值

---
+++ BinaryTree-二叉树/513. 找树左下角的值/层序/main.go
@@ -0,0 +1,40 @@
+package 层序
+
+import (
+	"LC/BinaryTree-二叉树/global"
+	"container/list"
+)
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/12/25
+ * Time: 20:51
+ * Description: No Description
+ */
+
+type TreeNode = global.TreeNode
+
+func findBottomLeftValue(root *TreeNode) int {
+	queue := list.New()
+	if root != nil {
+		queue.PushBack(root)
+	}
+	result := 0
+	for queue.Len() > 0 {
+		size := queue.Len()
+		for i := 0; i < size; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if i == 0 {
+				result = node.Val
+			}
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+	}
+	return result
+}
---
+++ BinaryTree-二叉树/513. 找树左下角的值/递归/main.go
@@ -0,0 +1,37 @@
+package 递归
+
+import (
+	"LC/BinaryTree-二叉树/global"
+)
+
+/**
+ * Created with GoLand 2021.3.2
+ * @author: 炸薯条
+ * Date: 2022/12/25
+ * Time: 21:09
+ * Description: No Description
+ */
+
+type TreeNode = global.TreeNode
+
+var depth int  // 全局变量 最大深度
+var result int // 记录最终结果
+
+func findBottomLeftValue(root *TreeNode) int {
+	depth, result = 0, 0 // 初始化
+	traversal(root, 1)
+	return result
+}
+
+func traversal(root *TreeNode, d int) {
+	if root == nil {
+		return
+	}
+	// 因为先遍历左边，所以左边如果有值，右边的同层不会更新结果
+	if root.Left == nil && root.Right == nil && depth < d {
+		depth = d
+		result = root.Val
+	}
+	traversal(root.Left, d+1)
+	traversal(root.Right, d+1)
+}
