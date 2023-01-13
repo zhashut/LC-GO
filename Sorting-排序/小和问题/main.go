136bdb8e01ff4ae0675006c8d61460cdd781bffb
2105798885@qq.com <2105798885@qq.com>
2023-01-13 20:42:40 +0800

    add 小和问题

--- Sorting-排序/小和问题/main.go
+++ Sorting-排序/小和问题/main.go
@@ -12,10 +12,10 @@ import "fmt"
 
 func main() {
 	arr := []int{6, 3, 2, 1, 6, 7}
-	fmt.Println(mergeSort(arr))
+	fmt.Println(smallSum(arr))
 }
 
-func mergeSort(arr []int) int {
+func smallSum(arr []int) int {
 	if len(arr) == 0 || len(arr) < 2 {
 		return 0
 	}
