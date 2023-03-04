c96f0100f5bd7796bb3150de04943a6f9f6f406e
Zhashut <2105798885@qq.com>
2023-03-04 22:24:09 +0800

    add LC-135. 分发糖果

--- Greedy-贪心/135. 分发糖果/main.go
+++ Greedy-贪心/135. 分发糖果/main.go
@@ -8,38 +8,44 @@ package _35__分发糖果
  * Description: No Description
  */
 
+/**
+ * Created with GoLand 2022.2.3.
+ * @author: 炸薯条
+ * Date: 2023/3/4
+ * Time: 21:46
+ * Description: https://leetcode.cn/problems/candy/
+ */
+
 func candy(ratings []int) int {
-	/**先确定一边，再确定另外一边
-	    1.先从左到右，当右边的大于左边的就加1
-	    2.再从右到左，当左边的大于右边的就再加1
-	**/
-	need := make([]int, len(ratings))
-	sum := 0
+	candyVec, sum := make([]int, len(ratings)), 0
+
 	// 初始化(每个人至少一个糖果)
 	for i := 0; i < len(ratings); i++ {
-		need[i] = 1
+		candyVec[i] = 1
 	}
 	// 1.先从左到右，当右边的大于左边的就加1
-	for i := 0; i < len(ratings)-1; i++ {
-		if ratings[i] < ratings[i+1] {
-			need[i+1] = need[i] + 1
+	for i := 1; i < len(ratings); i++ {
+		if ratings[i] > ratings[i-1] {
+			candyVec[i] = candyVec[i-1] + 1
 		}
 	}
 	// 2.再从右到左，当左边的大于右边的就右边加1，但要花费糖果最少，所以需要做下判断
-	for i := len(ratings) - 1; i > 0; i-- {
-		if ratings[i-1] > ratings[i] {
-			need[i-1] = findMax(need[i-1], need[i]+1)
+	for i := len(ratings) - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] {
+			candyVec[i] = max(candyVec[i], candyVec[i+1]+1)
 		}
 	}
 	//计算总共糖果
 	for i := 0; i < len(ratings); i++ {
-		sum += need[i]
+		sum += candyVec[i]
 	}
+
 	return sum
 }
-func findMax(num1 int, num2 int) int {
-	if num1 > num2 {
-		return num1
+
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return num2
+	return b
 }
