33e72fb6c743f470ef6dae2d855d40b8015bc19e
Zhashut <2105798885@qq.com>
2023-03-26 21:11:15 +0800

    add LC-198. 打家劫舍

---
+++ DynamicProgramming-动态规划/198. 打家劫舍/main.go
@@ -0,0 +1,74 @@
+package _98__打家劫舍
+
+/**
+ * Created with GoLand 2022.2.3.
+ * @author: 炸薯条
+ * Date: 2023/3/26
+ * Time: 20:52
+ * Description: https://leetcode.cn/problems/house-robber/
+ */
+
+func rob(nums []int) int {
+	n := len(nums)
+	dp := make([]int, n+1) // dp[i]表示偷到第i家能够偷得的最大金额
+	dp[1] = nums[0]
+	for i := 2; i <= n; i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
+	}
+	return dp[n]
+}
+
+// 解法一 DP
+func rob1(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return nums[0]
+	}
+	// dp[i] 代表抢 nums[0...i] 房子的最大价值
+	dp := make([]int, n)
+	dp[0], dp[1] = nums[0], max(nums[1], nums[0])
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+	}
+	return dp[n-1]
+}
+
+// 解法二 DP 优化辅助空间，把迭代的值保存在 2 个变量中
+func rob2(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	curMax, preMax := 0, 0
+	for i := 0; i < n; i++ {
+		tmp := curMax
+		curMax = max(curMax, nums[i]+preMax)
+		preMax = tmp
+	}
+	return curMax
+}
+
+// 解法三 模拟
+func rob3(nums []int) int {
+	// a 对于偶数位上的最大值的记录
+	// b 对于奇数位上的最大值的记录
+	a, b := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if i%2 == 0 {
+			a = max(a+nums[i], b)
+		} else {
+			b = max(a, b+nums[i])
+		}
+	}
+	return max(a, b)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
