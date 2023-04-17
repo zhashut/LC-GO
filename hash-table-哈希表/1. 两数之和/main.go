da408081a3dcfdd5f89a433fa56a14d78732c192
Zhashut <2105798885@qq.com>
2023-04-17 20:54:49 +0800

    add LC-1. 两数之和

--- hash-table-哈希表/1. 两数之和/main.go
+++ hash-table-哈希表/1. 两数之和/main.go
@@ -5,5 +5,18 @@ package ___两数之和
  * @author: 炸薯条
  * Date: 2023/4/17
  * Time: 20:48
- * Description: No Description
+ * Description: https://leetcode.cn/problems/two-sum/
  */
+
+func twoSum(nums []int, target int) []int {
+	containerMap := make(map[int]int)
+
+	for i, num := range nums {
+		if p, ok := containerMap[target-num]; ok {
+			return []int{i, p}
+		}
+		containerMap[num] = i
+	}
+
+	return nil
+}
