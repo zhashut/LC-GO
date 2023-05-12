97d7ac977379833de5d9582e3cdf070cc935272c
Zhashut <2105798885@qq.com>
2023-05-12 18:31:54 +0800

    add LC-90. 子集 II

--- backtracking-回溯/90. 子集 II/main.go
+++ backtracking-回溯/90. 子集 II/main.go
@@ -1,42 +1,38 @@
 package _0__子集_II
 
+import "sort"
+
 /**
  * Created with GoLand 2022.2.3.
  * @author: 炸薯条
  * Date: 2023/3/14
  * Time: 0:31
- * Description: No Description
+ * Description: https://leetcode.cn/problems/subsets-ii/
  */
 
 var (
-	res  [][]int
 	path []int
+	res  [][]int
 )
 
-func findSubsequences(nums []int) [][]int {
-	res, path = make([][]int, 0), make([]int, 0)
-
-	backtrack(nums, 0)
+func subsetsWithDup(nums []int) [][]int {
+	path, res = make([]int, 0, len(nums)), make([][]int, 0)
+	sort.Ints(nums)
+	dfs(nums, 0)
 	return res
 }
 
-func backtrack(nums []int, start int) {
-	if len(path) >= 2 {
-		tmp := make([]int, len(path))
-		copy(tmp, path)
-		res = append(res, tmp)
-	}
+func dfs(nums []int, start int) {
+	tmp := make([]int, len(path))
+	copy(tmp, path)
+	res = append(res, tmp)
 
-	used := make(map[int]bool, len(nums))
 	for i := start; i < len(nums); i++ {
-		if used[nums[i]] {
+		if i != start && nums[i] == nums[i-1] {
 			continue
 		}
-		if len(path) == 0 || nums[i] >= path[len(path)-1] {
-			path = append(path, nums[i])
-			used[nums[i]] = true
-			backtrack(nums, i+1)
-			path = path[:len(path)-1]
-		}
+		path = append(path, nums[i])
+		dfs(nums, i+1)
+		path = path[:len(path)-1]
 	}
 }
