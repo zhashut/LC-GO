ac336bece932e359f24d95ea86d63e2455d0b50b
Zhashut <2105798885@qq.com>
2023-07-11 08:29:54 +0800

    update LC-225. 用队列实现栈

--- Stack-栈/225. 用队列实现栈/main.go
+++ Stack-栈/225. 用队列实现栈/main.go
@@ -5,7 +5,7 @@ package main
  * @author: 炸薯条
  * Date: 2022/11/30
  * Time: 21:14
- * Description: No Description
+ * Description: https://leetcode.cn/problems/implement-stack-using-queues/
  */
 
 func main() {
@@ -14,87 +14,87 @@ func main() {
 
 //  -------------- 方法一,使用两个队列-----------------------------
 
-//type MyStack struct {
-//	Queue1 []int
-//	Queue2 []int
-//}
-//
-//func Constructor() MyStack {
-//	return MyStack{
-//		Queue1: make([]int, 0),
-//		Queue2: make([]int, 0),
-//	}
-//}
-//
-//func (this *MyStack) Push(x int) {
-//	this.Queue2 = append(this.Queue2, x)
-//	this.Move()
-//}
-//
-//func (this *MyStack) Move() {
-//	if len(this.Queue1) == 0 {
-//		this.Queue1, this.Queue2 = this.Queue2, this.Queue1
-//	} else {
-//		this.Queue2 = append(this.Queue2, this.Queue1[0])
-//		this.Queue1 = this.Queue1[1:]
-//		this.Move()
-//	}
-//}
-//
-//func (this *MyStack) Pop() int {
-//	val := this.Queue1[0]
-//	this.Queue1 = this.Queue1[1:]
-//	return val
-//}
-//
-//func (this *MyStack) Top() int {
-//	return this.Queue1[0]
-//}
-//
-//func (this *MyStack) Empty() bool {
-//	return len(this.Queue1) == 0
-//}
+type MyStack struct {
+	Queue1 []int
+	Queue2 []int
+}
+
+func Constructor() MyStack {
+	return MyStack{
+		Queue1: make([]int, 0),
+		Queue2: make([]int, 0),
+	}
+}
+
+func (this *MyStack) Push(x int) {
+	this.Queue2 = append(this.Queue2, x)
+	this.Move()
+}
+
+func (this *MyStack) Move() {
+	if len(this.Queue1) == 0 {
+		this.Queue1, this.Queue2 = this.Queue2, this.Queue1
+	} else {
+		this.Queue2 = append(this.Queue2, this.Queue1[0])
+		this.Queue1 = this.Queue1[1:]
+		this.Move()
+	}
+}
+
+func (this *MyStack) Pop() int {
+	val := this.Queue1[0]
+	this.Queue1 = this.Queue1[1:]
+	return val
+}
+
+func (this *MyStack) Top() int {
+	return this.Queue1[0]
+}
+
+func (this *MyStack) Empty() bool {
+	return len(this.Queue1) == 0
+}
 
 //  -------------- 方法一,使用两个队列-----------------------------
 
 //  -------------- 方法二,使用一个队列-----------------------------
 
-type MyStack struct {
-	Queue []int
-}
-
-func Constructor() MyStack {
-	return MyStack{
-		Queue: make([]int, 0),
-	}
-}
-
-func (this *MyStack) Push(x int) {
-	this.Queue = append(this.Queue, x)
-}
-
-func (this *MyStack) Pop() int {
-	n := len(this.Queue) - 1
-	for n != 0 {
-		val := this.Queue[0]
-		this.Queue = this.Queue[1:]
-		this.Queue = append(this.Queue, val)
-		n--
-	}
-	val := this.Queue[0]
-	this.Queue = this.Queue[1:]
-	return val
-}
-
-func (this *MyStack) Top() int {
-	val := this.Pop()
-	this.Queue = append(this.Queue, val)
-	return val
-}
-
-func (this *MyStack) Empty() bool {
-	return len(this.Queue) == 0
-}
+//type MyStack struct {
+//	Queue []int
+//}
+//
+//func Constructor() MyStack {
+//	return MyStack{
+//		Queue: make([]int, 0),
+//	}
+//}
+//
+//func (this *MyStack) Push(x int) {
+//	this.Queue = append(this.Queue, x)
+//}
+//
+//func (this *MyStack) Pop() int {
+//	n := len(this.Queue) - 1
+//	for n != 0 {
+//		val := this.Queue[0]
+//		this.Queue = this.Queue[1:]
+//		this.Queue = append(this.Queue, val)
+//		n--
+//	}
+//	val := this.Queue[0]
+//	this.Queue = this.Queue[1:]
+//	return val
+//}
+//
+//func (this *MyStack) Top() int {
+//	val := this.Pop()
+//	this.Queue = append(this.Queue, val)
+//	return val
+//}
+//
+//func (this *MyStack) Empty() bool {
+//	return len(this.Queue) == 0
+//}
 
 //  -------------- 方法二,使用一个队列-----------------------------
 
