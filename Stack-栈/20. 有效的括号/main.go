e836867c6bde39849efe1ea9827775b497d00bde
Zhashut <2105798885@qq.com>
2023-07-12 06:16:29 +0800

    update LC-20. 有效的括号

--- Stack-栈/20. 有效的括号/main.go
+++ Stack-栈/20. 有效的括号/main.go
@@ -5,7 +5,7 @@ package main
  * @author: 炸薯条
  * Date: 2022/11/28
  * Time: 22:37
- * Description: No Description
+ * Description: https://leetcode.cn/problems/valid-parentheses/
  */
 
 func main() {
